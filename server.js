73a365f6fb53312d054e7e9fcdaf9e0efdcaadf7
Robert Field <rfields03@gmail.com>
2022-03-19 19:52:02 -0400

    update files

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,10 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+const URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => console.log(err)
+);
 
 // routes
 app.use(require("./routes/api.js"));
