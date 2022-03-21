d1f6918f434eaae5f62bb082c9a53d225fde0848
Robert Field <rfields03@gmail.com>
2022-03-21 11:15:40 -0400

    trying to fix Mongo

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-const URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
 mongoose.connect(
   URI,
   {
